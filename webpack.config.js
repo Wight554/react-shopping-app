811c8edfb7632f2cbea371e7a161afdb0f927ad7
Volodymyr Zhdanov <wight554@gmail.com>
2019-07-20 16:28:17 +0300

    react-courses: rename to boilerplate
    * makes more sense to me

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'React Courses',
+      title: 'React Boilerplate',
       inject: true,
       favicon: './src/favicon.png',
       template: './src/index.ejs'

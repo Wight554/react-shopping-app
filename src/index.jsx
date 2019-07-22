f29d08a19838ef8a3237f5ec50b6b7f84347c32c
Volodymyr Zhdanov <wight554@gmail.com>
2019-07-22 19:20:32 +0300

    react-boilerplate: add react-intl

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { IntlProvider } from 'react-intl';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <IntlProvider locale="en">
+    <App />
+  </IntlProvider>,
+  document.getElementById('root')
+);

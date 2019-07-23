5947b324a800a15c0270b4b726fde8c6e5668b80
Volodymyr Zhdanov <wight554@gmail.com>
2019-07-23 11:57:06 +0300

    react-boilerplate: Add themes support

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 const displayName = 'App';
 
 export default function App() {
-  return <Route path="/" exact />;
+  return (
+    <ThemeProvider theme={{}}>
+      <Route path="/" exact />
+    </ThemeProvider>
+  );
 }
 
 App.displayName = displayName;
